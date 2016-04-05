e808a9c4b96a6002eb75481601036474888e6b34
Shaunak De <shaunak@gmail.com>
2016-04-05 15:58:59 +0530

    Shaunak

--- assets/js/top-notification.js
+++ assets/js/top-notification.js
@@ -1,20 +1,30 @@
 (function($){
 
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
+//var today = new Date();
+//var dd = today.getDate();
+//var mm = today.getMonth()+1; //January is 0!
+//var yyyy = today.getFullYear();
 
-if(dd<=27 && mm<=3 && yyyy==2016){
+$('div#alt').click(
+
+//alert("The selected participants for the workshop have been notified by email. This workshop is by invitation only. Thank you for your interest.")
+
+
+)
+
+//if(dd<=27 && mm<=3 && yyyy==2016){
 $('div#notice').slideDown(1000)
+
+
 
 $('span#close').click(
 
   function() {
-      $('div#notice').slideUp("slow")
+      $('div#notice').slideUp(1000)
+
     }
 
 )
-}
+//}
 })(jQuery);
